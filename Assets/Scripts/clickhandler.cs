5bdb43b9e21c70909eb095db8dba9106bc6c3a9a
Emad <sheikhemaduddin@gmail.com>
2020-05-11 18:25:13 +0500

    Added herbs and click script

---
+++ Assets/Scripts/clickhandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class clickhandler : MonoBehaviour, IPointerDownHandler
+{
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Clicked on herb!");
+    }
+
+}
---
+++ Assets/Scripts/elementbalance.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elementbalance : MonoBehaviour
+{
+    public int waterLevel = 50;
+    public int fireLevel = 80;
+    public int airLevel = 50;
+    int herbCount = 0;
+
+    void Update()
+    {
+        
+    }
+}
