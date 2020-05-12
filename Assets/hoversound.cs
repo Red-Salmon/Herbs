d87d1e0bc22f66b20d3c5b5aeec2a983271b18b4
Zack-Booth <garzazack@gmail.com>
2020-05-12 17:57:18 -0500

    added sounds to herbs and item/mixing windows

---
+++ Assets/hoversound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hoversound : MonoBehaviour
+{
+    [SerializeField]
+    private AK.Wwise.Event myEvent = null;
+
+    public void OnMouseEnter()
+    {
+        Debug.Log("Mouse is over GameObject.");
+        myEvent.Post(gameObject);
+    }
+
+}
