37eb4db1c9c5dafa9173399f8cf2468532caca80
Emad <sheikhemaduddin@gmail.com>
2020-05-12 23:47:53 +0500

    Added a next patient button
    The code for it is not yet complete.

--- Assets/Scripts/NextPatientScript.cs
+++ Assets/Scripts/NextPatientScript.cs
@@ -1,18 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class NextPatientScript : MonoBehaviour
+public class NextPatientScript : MonoBehaviour, IPointerDownHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform herbsContainer;
+    public Transform slotsContainer;
+
+    public Text patientCounterDisplay;
+    public int patientCounter = 1;
+
+    Transform[] HerbList;
+
+    mixingherbs mymixingherbs;
+
+    MixingHerbsSlot[] slotList;
+
+    public void Start()
     {
-        
+        mymixingherbs = mixingherbs.instance;
+        HerbList = herbsContainer.GetComponentsInChildren<Transform>();
+        slotList = slotsContainer.GetComponentsInChildren<MixingHerbsSlot>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerDown(PointerEventData eventData)
     {
-        
+        Debug.Log("Clicked on the next patient button");
+
+        // Updating the Patient Counter
+        patientCounter++;
+        patientCounterDisplay.text = "Patient # " + patientCounter;
+
+        // Resetting Herbs on display
+        for (int i = 0; i < HerbList.Length; i++)
+            HerbList[i].localScale = new Vector3(1f, 1f, 1f);
+
+        // Resetting Selected Herbs
+        mymixingherbs.herbList.Clear();
+        for (int i = 0; i < slotList.Length; i++)
+            slotList[i].ClearSlot();
+
+        // Introducing new Patient
+
     }
 }
