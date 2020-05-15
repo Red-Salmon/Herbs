af1ad1109c0be46120f0a79bf38f9f69bf944911
Emad <sheikhemaduddin@gmail.com>
2020-05-15 17:12:15 +0500

    Removing all Scripts
    These scripts will be added once again. Just fixing a build error.

--- Assets/Scripts/patient.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Patient", menuName = "Patient")]
-public class patient : ScriptableObject
-{
-    public Sprite healthy = null;
-    public Sprite sick = null;
-}
